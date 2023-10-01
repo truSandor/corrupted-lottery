d283a13499b205b33383d2adf9485f1b4c832b38
T.Sanyi <s.truczkai@gmail.com>
2023-10-01 20:29:59 +0200

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+/*
+     - we fill lottery tickets: [HOW_MANY] different numbers from 1-[UP_TO]
+     - generator makes [DATA_SIZE] random lottery tickets
+     - save the tickets in a file
+     - generate all possible combinations
+     - we gather the number combinations that are not in the randomly generated 'tickets'
+     - we check which one has the least [HOW_MANY - 1] - [HOW_MANY - 2] - [HOW_MANY - 3] matches
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
