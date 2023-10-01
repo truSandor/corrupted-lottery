96e7fbf308af696f6a0b82642a86fa9728680cf5
T.Sanyi <s.truczkai@gmail.com>
2023-10-01 20:36:05 +0200

    add main logic to create bought tickets

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,27 @@ package org.example;
      - we check which one has the least [HOW_MANY - 1] - [HOW_MANY - 2] - [HOW_MANY - 3] matches
  */
 
+import org.example.fileprocessor.FileProcessor;
+import org.example.generator.LotteryTicketGenerator;
+
 public class Main {
+    private static final int DATA_SIZE = 1000;
+    private static final int HOW_MANY = 5; // how many balls we pull
+    private static final int UP_TO = 20; // of how many numbers we pull
+    public static final String DATA_PATH = "src/main/resources/";
+    public static final String FILENAME = "bought-lottery-tickets.txt";
+    public static final String FILE_PATH = DATA_PATH + FILENAME;
+
     public static void main(String[] args) {
+        LotteryTicketGenerator generator = new LotteryTicketGenerator();
+        FileProcessor fileProcessor = new FileProcessor();
+
+        int[][] lotteryTickets = new int[DATA_SIZE][HOW_MANY];
+        for (int i = 0; i < DATA_SIZE; i++) {
+            lotteryTickets[i] = generator.generateLotteryTicket(HOW_MANY, UP_TO);
+        }
+        fileProcessor.writeToFile(FILE_PATH, lotteryTickets);
 
     }
+
 }
